bef34b2eebf751b17b1a41a4b3940334850e6b3a
Mercy <mercycherotich757@gmail.com>
2022-02-25 15:09:13 +0300

    add

--- config.py
+++ config.py
@@ -24,12 +24,6 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
 
     DEBUG = True
     
